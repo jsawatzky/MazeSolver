ec7b8326393a90e1fe75d6ad90e534c7177a992c
Jacob Sawatzky <jsawatzky@outlook.com>
2015-12-18 09:04:53 -0500

    Started work on learning Prim's algorithm

--- src/mazesolver/MazeSolver.java
+++ src/mazesolver/MazeSolver.java
@@ -1,6 +1,34 @@
 package mazesolver;
 
-public class MazeSolver {
+import java.awt.Graphics;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+
+public class MazeSolver extends JFrame {
+    
+    public ArrayList<Integer> grid = new ArrayList<>();
+    public ArrayList<int[]> frontier = new ArrayList<>();
+
+    public MazeSolver() {
+        
+        super("Maze");
+        
+        
+        
+    }
+    
+    public int[][] neighbors(x, y) {
+        
+        
+        
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        
+        
+        
+    }
 
     public static void main(String[] args) {
         
